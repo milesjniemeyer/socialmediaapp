f32bc0839ca614ba3d1247356f86b59ad67f2a86
Miles Niemeyer <milesniemeyer@MJN-MacBook-Pro.local>
2023-05-12 22:33:12 -0400

    updated cached files

--- app/routers/post.py
+++ app/routers/post.py
@@ -37,7 +37,7 @@ def get_post(id: int, db: Session = Depends(get_db)):
 
 # CREATE POST
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse)
-def create_posts(post: schemas.CreatePost, db: Session = Depends(get_db), get_current_user: int = Depends(oauth2.get_current_user)):
+def create_posts(post: schemas.CreatePost, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""", (post.title, post.content, post.published))
     # new_post = cursor.fetchone()
     # conn.commit()
